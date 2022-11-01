0e8becddbbf0657f4f8bd13df4ae95b551e711be
jahanabir8 <abir.opediatech@gmail.com>
2022-11-01 18:24:28 +0600

    master

---
+++ literal.js
@@ -0,0 +1,12 @@
+// Template literal
+// in simply way using simple concat
+var name = 'Abir Islam Anupom',
+ age = 23,
+  work = 'Student';
+console.log('I am ' + name + '. I am ' + age + ' years old. ' + ' I am a ' + work)
+
+// advance way using template literal ES6
+var name = 'Abir',
+age = 24,
+work = 'job'
+console.log(`I am ${name}. I am ${age} years old and i do a ${work}` )
